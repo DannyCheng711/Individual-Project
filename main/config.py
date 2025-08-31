ba0d7b7e1ec16c18da33f4ce824c8679d5a11b25
cc2224 <chieh-tung.cheng24@imperial.ac.uk>
2025-08-31 05:45:59 +0100

    fix/delete_nouse_files

--- main/config.py
+++
@@ -1,9 +0,0 @@
-import torch
-import os
-
-# Device config
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Dataset config
-DATASET_ROOT = "/vol/bitbucket/cc2224/cocodataset/coco-2017/"
-VOC_ROOT = "/vol/bitbucket/cc2224/voc/"
